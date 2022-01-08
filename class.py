8a0ed8fc649947518372cb9eee99ebcb47ff2ad6
priyankasharma090 <97334110+priyankasharma090@users.noreply.github.com>
2022-01-08 14:38:24 +0530

    Update class.py

--- class.py
+++ class.py
@@ -3,6 +3,10 @@ class area():
   self def rectarea(self, len , bre):
     self.len.len
     self.bre.bre
+    tri=len*bre
+    self def display()
+    print("area is" tri)
     
     
     a1=area()
+    a2=areaq()
